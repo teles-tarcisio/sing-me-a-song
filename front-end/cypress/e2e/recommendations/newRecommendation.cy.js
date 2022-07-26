8762d34488151ee8997307fd87ae9effe2bce266
Tarcísio F. S. Teles <tfst@ecomp.poli.br>
2022-07-26 20:57:07 -0300

    added cypress 'newRecommendation' test

--- front-end/cypress/e2e/recommendations/newRecommendation.cy.js
+++ front-end/cypress/e2e/recommendations/newRecommendation.cy.js
@@ -1,13 +1,24 @@
-import { faker } from "@faker-js/faker";
+// import { faker } from "@faker-js/faker";
 
-describe("Recommendations", () => {
-  it("should succesfully register a new video recommendation", () => {
-    const newVideo = {
-      name: faker.random.words(),
-      youtubeLink: `www.youtube.com/${faker.random.alpha(10)}`,
-    };
+const BASE_URL = 'http://localhost:3000';
 
-    cy.visit("http://localhost:3000");
+const songA = {
+  name: 'Casuarina: Ponto de Vista',
+  youtubeLink: 'https://www.youtube.com/watch?v=1dmQmMUdMt8',
+};
+
+describe('/home - send a song', () => {
+  it('should succesfully send a new song', () => {
+    cy.visit(BASE_URL);
+
+    cy.get('input[id="songName"]').type(songA.name);
+    cy.get('input[id="songLink"]').type(songA.youtubeLink);
+
+    cy.intercept('GET', '/recommendations').as('getRecommendations');
+
+    cy.get('button[id="sendSong"]').click();
+
+    cy.wait('@getRecommendations');
+    cy.get('article').contains(songA.name).should('be.visible');
   });
-
 });
