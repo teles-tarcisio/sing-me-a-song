e79cadab231c7eefed9316c67c340f4e2be2c381
Tarcísio F. S. Teles <tfst@ecomp.poli.br>
2022-07-29 12:03:37 -0300

    added 'duplicated recommendation name' test

--- back-end/tests/unit/recommendationsService.test.ts
+++ back-end/tests/unit/recommendationsService.test.ts
@@ -1,6 +1,7 @@
 import { jest } from "@jest/globals";
 import { CreateRecommendationData, recommendationService } from "../../src/services/recommendationsService.js";
 import { recommendationRepository } from "../../src/repositories/recommendationRepository.js";
+import { Recommendation } from "@prisma/client";
 
 jest.mock("../../src/repositories/recommendationRepository.js");
 
@@ -17,6 +18,31 @@ describe("recommendations unit test suite", () => {
     await recommendationService.insert(newRecommendation);
     expect(recommendationRepository.findByName).toBeCalledWith(newRecommendation.name);
     expect(recommendationRepository.create).toBeCalledWith(newRecommendation);
+  });
+
+  it("throws an error when trying to add recommendation with duplicated name", async () => {
+    const existentRecommendation: Recommendation = {
+      id: 12,
+      name: "existent name",
+      youtubeLink: "https://www.youtube.com/existent_recommendation",
+      score: 12,
+    };
+
+    jest.spyOn(recommendationRepository, "findByName").mockResolvedValueOnce(existentRecommendation);
+
+    const duplicatedRecommendation: CreateRecommendationData = {
+      name: existentRecommendation.name,
+      youtubeLink: existentRecommendation.youtubeLink,
+    };
+
+    const result = recommendationService.insert(duplicatedRecommendation);
+
+    expect(recommendationRepository.findByName)
+      .toBeCalledWith(duplicatedRecommendation.name);
+    expect(result)
+      .rejects
+      .toEqual({ type: "conflict", message: "Recommendations names must be unique"});
+
   });
 
 });
