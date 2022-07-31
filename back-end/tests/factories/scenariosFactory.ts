41ea9154cd467fc071d1919c5d74d057abffd675
Tarcísio F. S. Teles <tfst@ecomp.poli.br>
2022-07-30 21:02:03 -0300

    feat: finished integration tests for '/recommendations'

--- back-end/tests/factories/scenariosFactory.ts
+++ back-end/tests/factories/scenariosFactory.ts
@@ -1,7 +1,8 @@
+import { Recommendation } from "@prisma/client";
 import { prisma } from "../../src/database.js";
 import { CreateRecommendationData } from "../../src/services/recommendationsService.js";
 
-async function registerRecommendation(recommendation: CreateRecommendationData) {
+async function registerOneRecommendation(recommendation: CreateRecommendationData) {
   const addedRecommendation = await prisma.recommendation.create({
     data: {
       name: recommendation.name,
@@ -11,8 +12,49 @@ async function registerRecommendation(recommendation: CreateRecommendationData) 
   return addedRecommendation;  
 }
 
+function __generateRecommendationsArray(size: number): CreateRecommendationData[] {
+  const recommendationsArray: CreateRecommendationData[] = [];
+  for (let i = 0; i < size; i++) {
+    recommendationsArray.push({
+      name: `${i}`,
+      youtubeLink: `https://www.youtube.com/link${i}`,
+    });
+  }
+
+  return recommendationsArray;
+}
+
+async function addRecommendationsScenario(size: number) {
+  const recommendationsArray = __generateRecommendationsArray(size);
+
+  await prisma.recommendation.createMany({
+    data: recommendationsArray,
+    skipDuplicates: true,
+  });
+}
+
+async function addScoredRecommendationsScenario(size: number) {
+  const recommendationsArray = __generateRecommendationsArray(size);
+  recommendationsArray
+
+  recommendationsArray.forEach(async (video, index) => {
+    await prisma.recommendation.upsert({
+      where: { name: video.name },
+      update: {},
+      create: {
+        name: video.name,
+        youtubeLink: video.youtubeLink,
+        score: index + 50,
+      },
+    });  
+  });
+
+}
+
 const scenariosFactory = {
-  registerRecommendation,
+  registerOneRecommendation,
+  addRecommendationsScenario,
+  addScoredRecommendationsScenario,
 };
 
 export default scenariosFactory;
--- back-end/tests/integration/app.test.ts
+++ back-end/tests/integration/app.test.ts
@@ -1,6 +1,7 @@
 import app from "../../src/app.js";
 import supertest from "supertest";
 import { prisma } from "../../src/database.js";
+import { Recommendation } from "@prisma/client";
 import recommendationFactory from "../factories/recommendationFactory.js";
 import scenariosFactory from "../factories/scenariosFactory.js";
 import dotenv from "dotenv";
@@ -11,7 +12,7 @@ const BASE_URL = "/recommendations";
 const APP = supertest(app);
 
 beforeEach(async () => {
-  await prisma.recommendation.deleteMany();
+  await prisma.$executeRaw`TRUNCATE TABLE recommendations RESTART IDENTITY;`;
 })
 
 describe("POST /recommendations test suite", () => {
@@ -63,13 +64,13 @@ describe("POST /recommendations test suite", () => {
 
   it("correctly increments a recommendation score", async () => {
     const newRecommendation = recommendationFactory.createFakeRecommendation();
-    const insertedRecommendation = await scenariosFactory.registerRecommendation(newRecommendation);
+    const insertedRecommendation = await scenariosFactory.registerOneRecommendation(newRecommendation);
     const targetId = insertedRecommendation.id;
     const previousScore = insertedRecommendation.score;
-    
+
     const response = await APP.post(BASE_URL + `/${targetId}/upvote`);
     expect(response.statusCode).toBe(200);
-    
+
     const updatedScore = await prisma.recommendation.findUnique({
       where: {
         id: insertedRecommendation.id,
@@ -80,13 +81,13 @@ describe("POST /recommendations test suite", () => {
 
   it("correctly decrements a recommendation score", async () => {
     const newRecommendation = recommendationFactory.createFakeRecommendation();
-    const insertedRecommendation = await scenariosFactory.registerRecommendation(newRecommendation);
+    const insertedRecommendation = await scenariosFactory.registerOneRecommendation(newRecommendation);
     const targetId = insertedRecommendation.id;
     const previousScore = insertedRecommendation.score;
-    
+
     const response = await APP.post(BASE_URL + `/${targetId}/downvote`);
     expect(response.statusCode).toBe(200);
-    
+
     const updatedScore = await prisma.recommendation.findUnique({
       where: {
         id: insertedRecommendation.id,
@@ -97,20 +98,87 @@ describe("POST /recommendations test suite", () => {
 });
 
 
-describe("GET /recommendations", () => {
-  it.todo("correctly receives 10/10 recommendations");
-  it.todo("correctly receives 7/10 recommendations");
-  it.todo("correctly receives 10/13 recommendations");
+describe("GET /recommendations test suite", () => {
+  it("correctly receives 10/13 recommendations", async () => {
+    const recommendationsArray13 = await scenariosFactory.addRecommendationsScenario(13);
 
-  it.todo("correctly receives recommendation by id");
-  it.todo("returns XYZ for recommendation with invalid id");
+    const response = await APP.get(BASE_URL);
+    const size = response.body.length;
+    expect(size).toEqual(10);
+  });
 
-  it.todo("correctly receives random recommendation");
-  it.todo("returns 404 for 'random' but without existent recommendations");
+  it("correctly receives 07/10 recommendations", async () => {
+    const recommendationsArray7 = await scenariosFactory.addRecommendationsScenario(7);
 
-  it.todo("correctly receives sorted top X recommendations");
+    const response = await APP.get(BASE_URL);
+    const size = response.body.length;
+    expect(size).toEqual(7);
+  });
+
+  it("correctly receives recommendation by id", async () => {
+    await scenariosFactory.addRecommendationsScenario(12);
+    const targetId = 9;
+
+    const response = await APP.get(BASE_URL + `/${targetId}`);
+    expect(response.body.id).toEqual(targetId);
+  });
+
+  it("returns 404 for recommendation with inexistent id", async () => {
+    await scenariosFactory.addRecommendationsScenario(4);
+    const targetId = 6;
+
+    const response = await APP.get(BASE_URL + `/${targetId}`);
+    expect(response.statusCode).toBe(404);
+  });
+
+  it("returns 500 for recommendation with invalid id parameter", async () => {
+    const targetId = "Z";
+    const response = await APP.get(BASE_URL + `/${targetId}`);
+    expect(response.statusCode).toBe(500);
+  });
+
+  it("correctly receives random recommendation", async () => {
+    await scenariosFactory.addRecommendationsScenario(14);
+
+    const response = await APP.get(BASE_URL + "/random");
+    const randomRecommendation: Recommendation = response.body;
+    expect(randomRecommendation.id).toBeGreaterThanOrEqual(1);
+    expect(randomRecommendation.id).toBeLessThanOrEqual(14);
+
+  });
+
+  it("returns 404 for '/random' but without existent recommendations", async () => {
+    const response = await APP.get(BASE_URL + "/random");
+    expect(response.statusCode).toBe(404);
+    expect(response.body).toEqual({});
+  });
+
+  it("correctly receives sorted top X recommendations", async () => {
+    const amount = 6;
+    const total = 15;
+    await scenariosFactory.addScoredRecommendationsScenario(total);
+
+    const response = await APP.get(BASE_URL + `/top/${amount}`);
+    const recommendationsArray = response.body;
+    expect(recommendationsArray.length).toEqual(amount);
+    
+    const sorted = __checkScoreSorted(recommendationsArray);
+    expect(sorted).toEqual(true);
+  });
 
 });
+
+function __checkScoreSorted(targetArray: any[]): boolean {
+  let sorted = true;
+  for (let i = 0; i < targetArray.length - 1; i++) {
+    if (targetArray[i].score < targetArray[i + 1].score) {
+      sorted = false;
+      break;
+    }
+  }
+
+  return sorted;
+}
 
 afterAll(async () => {
   await prisma.$disconnect();
