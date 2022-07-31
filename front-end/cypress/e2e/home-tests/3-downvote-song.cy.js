73f3402665af2190055781b477283bfb903eef74
Tarcísio F. S. Teles <tfst@ecomp.poli.br>
2022-07-31 14:38:06 -0300

    added 'downvote song' test

---
+++ front-end/cypress/e2e/home-tests/3-downvote-song.cy.js
@@ -0,0 +1,44 @@
+/// <reference types="cypress" />
+
+const BASE_URL = 'http://localhost:3000';
+const API_URL = 'http://localhost:5000';
+
+const newSong = {
+  name: 'Strange Dark House Mix',
+  youtubeLink: 'https://www.youtube.com/watch?v=98a_uq0r7RQ',
+};
+
+beforeEach(() => {
+  cy.resetDatabase();
+});
+
+describe("/home - downvote a video", () => {
+  it("should succesfully downvote a video", () => {
+    cy.visit(BASE_URL);
+    cy.request("POST", `${API_URL}/recommendations`, newSong).then((response) => {
+      console.log(response.body, response.status);
+    });
+
+    cy.intercept("GET", `${API_URL}/recommendations`).as("getRecommendations");
+    cy.wait("@getRecommendations");
+
+    cy.get("article").contains(newSong.name)
+      .should("be.visible")
+      .parent()
+      .find("div:nth-child(3)")
+      .contains("0");
+
+    cy.get("article").contains(newSong.name)
+      .should('be.visible')
+      .parent()
+      .find('div:nth-child(3)')
+      .find('svg:nth-child(2)')
+      .click();
+
+    cy.get('article').contains(newSong.name)
+      .should('be.visible')
+      .parent()
+      .find('div:nth-child(3)')
+      .contains('-1');
+  });
+});
