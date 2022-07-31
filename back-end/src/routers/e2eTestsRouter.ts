d63c94d4af1cd369f3c464c65472f28b15a15c16
Tarcísio F. S. Teles <tfst@ecomp.poli.br>
2022-07-31 14:57:43 -0300

    chore: added endpoint to populate database for e2e tests

--- back-end/src/routers/e2eTestsRouter.ts
+++ back-end/src/routers/e2eTestsRouter.ts
@@ -1,7 +1,9 @@
 import { Router } from "express";
 import { e2eTestsController } from "../controllers/e2eTestsController.js";
+import { recommendationController } from "../controllers/recommendationController.js";
 
 const e2eRouter = Router();
 e2eRouter.post("/tests/reset-database", e2eTestsController.deleteRecommendations);
+e2eRouter.post("/tests/populate-top", recommendationController.insert);
 
 export default e2eRouter;
